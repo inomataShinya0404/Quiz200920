974d7e5c5f836bd3c0dd46b85e1b41bb84ee1ce4
Shinya Inomata <shinya.inomata@gmail.com>
2020-09-21 01:44:46 +0900

    Initial Commit

---
+++ Quiz200920/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quiz200920
+//
+//  Created by Shinya  on 2020/09/21.
+//  Copyright © 2020 Shinya . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
